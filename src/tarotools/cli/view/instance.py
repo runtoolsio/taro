9e6a0f3b1961db2195e92ff57344a31299b00a72
Stan Svec <stan@taggstar.com>
2023-10-29 23:03:15 +0700

    #90: lifecycle tests

--- src/tarotools/cli/view/instance.py
+++ src/tarotools/cli/view/instance.py
@@ -1,5 +1,6 @@
 from tarotools.cli.printer import Column
 from tarotools.cli.style import general_style, job_id_style, instance_style, warn_style, job_state_style
+
 from tarotools.taro import util, TerminationStatus
 from tarotools.taro.util import format_dt_local_tz
 
@@ -10,7 +11,7 @@ PARAMETERS = Column('PARAMETERS', 23,
 CREATED = Column('CREATED', 25, lambda j: format_dt_local_tz(j.lifecycle.created_at), general_style)
 EXECUTED = Column('EXECUTED', 25, lambda j: format_dt_local_tz(j.lifecycle.executed_at, null='N/A'), general_style)
 ENDED = Column('ENDED', 25, lambda j: format_dt_local_tz(j.lifecycle.ended_at, null='N/A'), general_style)
-EXEC_TIME = Column('TIME', 18, lambda j: util.format_timedelta(j.lifecycle.execution_time, show_ms=False, null='N/A'),
+EXEC_TIME = Column('TIME', 18, lambda j: util.format_timedelta(j.lifecycle.total_executing_time, show_ms=False, null='N/A'),
                    general_style)
 STATE = Column('STATE', max(len(s.name) for s in TerminationStatus) + 2, lambda j: j.phase.name, job_state_style)
 WARNINGS = Column('WARNINGS', 40, lambda j: ', '.join("{}: {}".format(k, v) for k, v in j.warnings.items()), warn_style)
