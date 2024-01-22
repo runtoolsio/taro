e0f4ac31ffcdcda3058cf21582bba91a3c4cd00e
Stan Svec <stan@taggstar.com>
2024-01-22 10:40:53 +0100

    history remove ok

--- src/runtoolsio/taro/cmd/history_remove.py
+++ src/runtoolsio/taro/cmd/history_remove.py
@@ -8,7 +8,7 @@ from runtoolsio.taro import cliutil
 def run(args):
     total = 0
     for instance in args.instances:
-        run_match = EntityRunCriteria.parse_pattern(instance, MatchingStrategy.FN_MATCH)
+        run_match = EntityRunCriteria.from_instance_pattern(instance, MatchingStrategy.FN_MATCH)
         count = persistence.count_instances(instance_match=run_match)
         print(str(count) + " records found for " + instance)
         total += count
@@ -18,5 +18,5 @@ def run(args):
         return
 
     for instance in args.instances:
-        run_match = EntityRunCriteria.parse_pattern(instance, MatchingStrategy.FN_MATCH)
-        persistence.remove_instances(run_match)
+        run_match = EntityRunCriteria.from_instance_pattern(instance, MatchingStrategy.FN_MATCH)
+        persistence.remove_runs(run_match)
