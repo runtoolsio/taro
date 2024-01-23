ac06d1cc1f78fc369906c51a5576a2bf9e4d2827
Stan Svec <stan@taggstar.com>
2024-01-23 10:55:12 +0100

    refactor rename

--- src/runtoolsio/taro/cmd/listen.py
+++ src/runtoolsio/taro/cmd/listen.py
@@ -12,13 +12,13 @@ from runtoolsio.taro import printer, style, cliutil
 def run(args):
     instance_match = compound_instance_filter(argsutil.instance_criteria(args, MatchingStrategy.PARTIAL))
     receiver = InstanceTransitionReceiver(instance_match)
-    receiver.add_observer_transition(EventPrint(receiver, args.timestamp.value))
+    receiver.add_observer_transition(TransitionPrint(receiver, args.timestamp.value))
     receiver.start()
     cliutil.exit_on_signal(cleanups=[receiver.close_and_wait])
     receiver.wait()  # Prevents 'exception ignored in: <module 'threading' from ...>` error message
 
 
-class EventPrint(InstanceTransitionObserver):
+class TransitionPrint(InstanceTransitionObserver):
 
     def __init__(self, receiver, ts_format=DateTimeFormat.DATE_TIME_MS_LOCAL_ZONE):
         self._receiver = receiver
@@ -26,8 +26,8 @@ class EventPrint(InstanceTransitionObserver):
 
     def new_instance_phase(self, job_run: JobRun, previous_phase: PhaseRun, new_phase: PhaseRun, ordinal: int):
         try:
-            styled_texts = style.run_status_line(job_run, ts_prefix_format=self.ts_format)
-            printer.print_styled(*styled_texts)
+            styled_status_line = style.run_status_line(job_run, ts_prefix_format=self.ts_format)
+            printer.print_styled(*styled_status_line)
             sys.stdout.flush()
         except BrokenPipeError:
             self._receiver.close_and_wait()
