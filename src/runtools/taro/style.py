6d8bc2f53604972469673152ca5dc15ed340ea6f
stan <stan@taggstar.com>
2024-11-02 14:47:44 +0000

    run encapsulated in job run

--- src/runtools/taro/style.py
+++ src/runtools/taro/style.py
@@ -6,7 +6,7 @@ from runtools.taro.theme import Theme
 
 
 def job_id_style(job):
-    if job.run.termination and job.run.termination.status.is_outcome(Outcome.FAULT):
+    if job.termination and job.termination.status.is_outcome(Outcome.FAULT):
         return Theme.job + " " + Theme.state_failure
     return Theme.job
 
@@ -18,13 +18,13 @@ def job_id_stats_style(job_stats):
 
 
 def instance_style(job):
-    if job.run.termination and job.run.termination.status.is_outcome(Outcome.FAULT):
+    if job.termination and job.termination.status.is_outcome(Outcome.FAULT):
         return Theme.state_failure
     return Theme.instance
 
 
 def general_style(job):
-    if job.run.termination and job.run.termination.status.is_outcome(Outcome.FAULT):
+    if job.termination and job.termination.status.is_outcome(Outcome.FAULT):
         return Theme.state_failure
     return ""
 
@@ -40,7 +40,7 @@ def warn_style(_):
 
 
 def job_state_style(job):
-    return state_style(job.run.lifecycle.run_state)
+    return state_style(job.lifecycle.run_state)
 
 
 def term_style(term_status) -> str:
@@ -56,7 +56,7 @@ def term_style(term_status) -> str:
 
 
 def run_term_style(entity_run) -> str:
-    return term_style(entity_run.run.termination.status)
+    return term_style(entity_run.termination.status)
 
 
 def stats_state_style(stats):
@@ -101,12 +101,12 @@ def entity_run_id(metadata) -> List[Tuple[str, str]]:
 
 
 def run_status_line(entity_run, *, ts_prefix_format=DateTimeFormat.DATE_TIME_MS_LOCAL_ZONE) -> List[Tuple[str, str]]:
-    if entity_run.run.termination:
-        status = (run_term_style(entity_run), entity_run.run.termination.status.name)
+    if entity_run.termination:
+        status = (run_term_style(entity_run), entity_run.termination.status.name)
     else:
-        status = (state_style(entity_run.run.lifecycle.run_state), entity_run.run.lifecycle.run_state.name)
+        status = (state_style(entity_run.lifecycle.run_state), entity_run.lifecycle.run_state.name)
     styled_texts = entity_run_id(entity_run.metadata) + [("", " -> "), status]
-    ts = entity_run.run.lifecycle.last_transition_at
+    ts = entity_run.lifecycle.last_transition_at
     ts_prefix_formatted = ts_prefix_format(ts) if ts else None
     if ts_prefix_formatted:
         return [("", ts_prefix_formatted + " ")] + styled_texts
