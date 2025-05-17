711ed6e3212bc8a5d87e5c1dcb897bdff4523076
stan svec <dev@stansvec.com>
2025-05-17 11:52:54 +0700

    env option

--- src/runtools/taro/cmd/approve.py
+++ src/runtools/taro/cmd/approve.py
@@ -1,17 +1,27 @@
-from typing import List
+from typing import List, Optional
 
 import typer
 
 app = typer.Typer(invoke_without_command=True)
 
 
+def env_option() -> Optional[str]:
+    return typer.Option(
+        None,
+        "--env", "-e",
+        help="Target environment (e.g. dev, prod)"
+    )
+
+
 @app.callback()
 def approve(
         phase: str = typer.Option(..., "--phase", "-p", help="Phase ID"),
         instance_ids: List[str] = typer.Argument(..., help="One or more instance IDs"),
+        env: Optional[str] = env_option(),
 ):
     typer.echo(f"Phase: {phase}")
     typer.echo(f"Instance IDs: {', '.join(instance_ids)}")
+    print(env)
 
 # def run(args):
 #     run_match = argsutil.run_criteria(args, MatchingStrategy.FN_MATCH)
