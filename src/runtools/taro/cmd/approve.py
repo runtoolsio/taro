d60af3d0a897e9527c75bde8ace90b991a804586
stan <stan@taggstar.com>
2025-05-17 23:27:53 +0700

    resolve env configs logic

--- src/runtools/taro/cmd/approve.py
+++ src/runtools/taro/cmd/approve.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import typer
 from runtools.runcore import env
@@ -8,27 +8,53 @@ from runtools.runcore.paths import ConfigFileNotFoundError
 app = typer.Typer(invoke_without_command=True)
 
 
-def env_option() -> Optional[str]:
-    return typer.Option(
-        None,
-        "--env", "-e",
-        help="Target environment (e.g. dev, prod)"
-    )
+def env_option():
+    return typer.Option((), "--env", "-e", help="Target environment")
+
+
+def resolve_env_configs(*env_ids):
+    """
+    Load environment configurations with fallback for missing local environment.
+
+    Behavior:
+    1. No environments specified:
+        - Load all available environments.
+        - Add default `local` environment if it is not found in the loaded configs.
+    2. Environments specified and includes `local`:
+        - Load specified environments.
+        - Add default `local` environment if it is not found in the loaded configs.
+    3. Environments specified and does NOT include `local`:
+        - Load only the specified environments.
+    """
+    env_ids_set = set(env_ids)
+    if DEFAULT_LOCAL_ENVIRONMENT in env_ids_set and len(env_ids_set) > 1:
+        explicit_local = True
+        env_ids_set.remove(DEFAULT_LOCAL_ENVIRONMENT)
+    else:
+        explicit_local = False
+
+    try:
+        env_configs = env.get_env_configs(*env_ids_set)
+    except (ConfigFileNotFoundError, EnvironmentNotFoundError) as e:
+        if env_ids_set and env_ids_set != {DEFAULT_LOCAL_ENVIRONMENT}:
+            raise e
+        return {DEFAULT_LOCAL_ENVIRONMENT: env.get_default_config(DEFAULT_LOCAL_ENVIRONMENT)}
+
+    if (not env_ids_set or explicit_local) and DEFAULT_LOCAL_ENVIRONMENT not in env_configs:
+        env_configs[DEFAULT_LOCAL_ENVIRONMENT] = env.get_default_config(DEFAULT_LOCAL_ENVIRONMENT)
+
+    return env_configs
 
 
 @app.callback()
 def approve(
         phase: str = typer.Option(..., "--phase", "-p", help="Phase ID"),
         instance_ids: List[str] = typer.Argument(..., help="One or more instance IDs"),
-        env_id: Optional[str] = env_option(),
+        env_ids: List[str] = env_option(),
 ):
-    try:
-        env_config, _ = env.load_env_config(env_id)
-    except (ConfigFileNotFoundError, EnvironmentNotFoundError) as e:
-        if env_id == DEFAULT_LOCAL_ENVIRONMENT:
-            env_config, _ = env.load_env_default_config(env_id)
-        else:
-            raise e
+    env_configs = resolve_env_configs(*env_ids)
+
+    print(env_configs)
 
 # def run(args):
 #     run_match = argsutil.run_criteria(args, MatchingStrategy.FN_MATCH)
