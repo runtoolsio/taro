450510735a19d847319d3872df9e865e11d4955f
stan svec <dev@stansvec.com>
2025-05-20 08:26:54 +0700

    resolve env config moved to runcore

--- src/runtools/taro/cmd/approve.py
+++ src/runtools/taro/cmd/approve.py
@@ -1,52 +1,17 @@
-from typing import List, Dict
+from typing import List
 
 import typer
 from rich.console import Console
 
-from runtools.runcore import env, connector
+from runtools.runcore import connector
 from runtools.runcore.criteria import JobRunCriteria
-from runtools.runcore.env import EnvironmentNotFoundError, DEFAULT_LOCAL_ENVIRONMENT, EnvironmentConfigUnion
-from runtools.runcore.paths import ConfigFileNotFoundError
+from runtools.runcore.env import resolve_env_configs
 
 app = typer.Typer(invoke_without_command=True)
 console = Console()
 
 def env_option():
     return typer.Option((), "--env", "-e", help="Target environment")
-
-
-def resolve_env_configs(*env_ids) -> Dict[str, EnvironmentConfigUnion]:
-    """
-    Load environment configurations with fallback for missing local environment.
-
-    Behavior:
-    1. No environments specified:
-        - Load all available environments.
-        - Add default `local` environment if it is not found in the loaded configs.
-    2. Environments specified and includes `local`:
-        - Load specified environments.
-        - Add default `local` environment if it is not found in the loaded configs.
-    3. Environments specified and does NOT include `local`:
-        - Load only the specified environments.
-    """
-    env_ids_set = set(env_ids)
-    if DEFAULT_LOCAL_ENVIRONMENT in env_ids_set and len(env_ids_set) > 1:
-        mixed_explicit_local = True
-        env_ids_set.remove(DEFAULT_LOCAL_ENVIRONMENT)
-    else:
-        mixed_explicit_local = False
-
-    try:
-        env_configs = env.get_env_configs(*env_ids_set)
-    except (ConfigFileNotFoundError, EnvironmentNotFoundError) as e:
-        if env_ids_set and env_ids_set != {DEFAULT_LOCAL_ENVIRONMENT}:
-            raise e
-        return {DEFAULT_LOCAL_ENVIRONMENT: env.get_default_config(DEFAULT_LOCAL_ENVIRONMENT)}
-
-    if (not env_ids_set or mixed_explicit_local) and DEFAULT_LOCAL_ENVIRONMENT not in env_configs:
-        env_configs[DEFAULT_LOCAL_ENVIRONMENT] = env.get_default_config(DEFAULT_LOCAL_ENVIRONMENT)
-
-    return env_configs
 
 
 @app.callback()
