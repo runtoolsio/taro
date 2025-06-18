8dc9954e9d4d4aea5eedfe22fd6a3d51d18db7ce
stan <stan@taggstar.com>
2025-06-18 09:34:00 +0700

    hostinfo command deleted

--- src/runtools/taro/cmd/hostinfo.py
+++
@@ -1,8 +0,0 @@
-import runtools.runcore.util.hostinfo
-
-
-def run(args):
-    # Use `show` argument: taro host info show
-    host_info = runtools.runcore.util.hostinfo.read_hostinfo()
-    for name, value in host_info.items():
-        print(f"{name}: {value}")
