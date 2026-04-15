61d0f18ab318477ab4b677a0e60fbe854c505693
stan <stan@taggstar.com>
2026-04-15 10:17:40 +0700

    taro stats deleted

--- src/runtools/taro/main.py
+++ src/runtools/taro/main.py
@@ -6,7 +6,7 @@ from rich.text import Text
 
 from runtools.runcore.err import RuntoolsException
 from runtools.taro import __version__
-from runtools.taro.cmd import approve, clean, dash, env, history, inspect, jobs, listen, live, of, ps, resume, stats, stop, tail, wait
+from runtools.taro.cmd import approve, clean, dash, env, history, inspect, jobs, listen, live, of, ps, resume, stop, tail, wait
 
 console = Console(stderr=True)
 
@@ -37,7 +37,6 @@ app.add_typer(live.app, name="live")
 app.add_typer(of.app, name="of")
 app.add_typer(ps.app, name="ps")
 app.add_typer(resume.app, name="resume")
-app.add_typer(stats.app, name="stats")
 app.add_typer(stop.app, name="stop")
 app.add_typer(tail.app, name="tail")
 app.add_typer(wait.app, name="wait")
@@ -47,7 +46,6 @@ app.add_typer(dash.app, name="d", hidden=True)
 app.add_typer(history.app, name="h", hidden=True)
 app.add_typer(jobs.app, name="j", hidden=True)
 app.add_typer(live.app, name="l", hidden=True)
-app.add_typer(stats.app, name="s", hidden=True)
 app.add_typer(tail.app, name="t", hidden=True)
 app.add_typer(stop.app, name="x", hidden=True)
 
